7f1d8912b9d9e7d758807c892614bebd63c76784
Gil Benezer <gil.benezer@gmail.com>
2025-03-12 01:09:15 -0400

    more notes

--- src/network/evaluate_network.py
+++ src/network/evaluate_network.py
@@ -5,3 +5,5 @@
 # and inference memory performance as three separate objective functions
 # Inference time and memory utilization are subject to more noise, so that may be a consideration
 
+# TODO: find way to extract metrics from TensorBoard and other logs
+# NOTE: naming/organization of lightning log directory likely needs to be handled by Ax/BoTorch portion
