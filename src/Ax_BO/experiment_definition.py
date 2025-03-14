f8907f9a0098b972d0b6b63c2fd8629e88f31e84
Gil Benezer <gil.benezer@gmail.com>
2025-03-14 14:31:15 -0400

    added variables encapsulating the superconductivity critical temperature regression experiments, including the definition of standard status quo hyperparameters for testing

--- src/Ax_BO/experiment_definition.py
+++ src/Ax_BO/experiment_definition.py
@@ -7,8 +7,36 @@ logging.getLogger("ax.service").setLevel(logging.WARNING)
 logging.getLogger("ax.modelbridge").setLevel(logging.WARNING)
 logging.getLogger("ax.modelbridge.dispatch_utils").setLevel(logging.WARNING)
 logging.getLogger("ax.service.utils.instantiation").setLevel(logging.WARNING)
+logging.getLogger("ax.service.ax_client").setLevel(logging.WARNING)
 
-ax_client = AxClient(verbose_logging=False)
+ax_client = AxClient()
+
+MNIST_status_quo_parameters = {
+    "input_dropout_probability": 0.1,
+    "hidden_dropout_probability": 0.5,
+    "output_dropout_probability": 0.1,
+    "hidden_layer_nodes_1": 500,
+    "hidden_layer_nodes_2": 100,
+    "hidden_layer_nodes_3": 50,
+    "activation": "relu",
+    "learning_rate": 1e-3,
+    "beta1": 0.9,
+    "beta2": 0.999,
+    "w_decay": 0,
+}
+Superconductivity_status_quo_parameters = {
+    "input_dropout_probability": 0.1,
+    "hidden_dropout_probability": 0.2,
+    "output_dropout_probability": 0.1,
+    "hidden_layer_nodes_1": 27,
+    "hidden_layer_nodes_2": 9,
+    "hidden_layer_nodes_3": 3,
+    "activation": "relu",
+    "learning_rate": 1e-3,
+    "beta1": 0.9,
+    "beta2": 0.999,
+    "w_decay": 0,
+}
 
 MNIST_parameters = [
     {
@@ -91,7 +119,87 @@ MNIST_parameters = [
     },
 ]
 
-# TODO: define hyperparameter domain for regression problem
+Superconductivity_parameters = [
+    {
+        "name": "input_dropout_probability",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "hidden_dropout_probability",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "output_dropout_probability",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "hidden_layer_nodes_1",
+        "type": "range",
+        "bounds": [10, 200],
+        "value_type": "int",
+        "log_scale": False,
+    },
+    {
+        "name": "hidden_layer_nodes_2",
+        "type": "range",
+        "bounds": [10, 200],
+        "value_type": "int",
+        "log_scale": False,
+    },
+    {
+        "name": "hidden_layer_nodes_3",
+        "type": "range",
+        "bounds": [10, 200],
+        "value_type": "int",
+        "log_scale": False,
+    },
+    {
+        "name": "learning_rate",
+        "type": "range",
+        "bounds": [1e-8, 1.0],
+        "value_type": "float",
+        "log_scale": True,
+    },
+    {
+        "name": "beta1",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "beta2",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "w_decay",
+        "type": "range",
+        "bounds": [0.0, 1.0],
+        "value_type": "float",
+        "log_scale": False,
+    },
+    {
+        "name": "activation",
+        "type": "choice",
+        "values": ["swish", "sigmoid", "relu", "leaky_relu"],
+        "value_type": "str",
+        "is_ordered": False,
+        "sort_values": False,
+    },
+]
+
 budget_variables = [
     {
         "name": "max_epochs",
@@ -112,6 +220,9 @@ budget_variables = [
 MNIST_parameters_w_budget = MNIST_parameters + budget_variables
 
 MNIST_single_objective = {"test_accuracy": ObjectiveProperties(minimize=False)}
+Superconductivity_single_objective = {
+    "test_nrmse_mean": ObjectiveProperties(minimize=True)
+}
 
 MNIST_multiobjective = {
     "test_accuracy": ObjectiveProperties(minimize=False, threshold=0.9),
@@ -120,17 +231,24 @@ MNIST_multiobjective = {
     "checkpoint_size": ObjectiveProperties(minimize=True),
 }
 
-p_constraints = [
-        "hidden_layer_nodes_2 <= hidden_layer_nodes_1",
-        "hidden_layer_nodes_3 <= hidden_layer_nodes_2",
-    ]
+Superconductivity_multiobjective = {
+    "test_nrmse_mean": ObjectiveProperties(minimize=True),
+    "number_parameters": ObjectiveProperties(minimize=True),
+    "training_time": ObjectiveProperties(minimize=True),
+    "checkpoint_size": ObjectiveProperties(minimize=True),
+}
 
-o_constraints=None
+p_constraints = [
+    "hidden_layer_nodes_2 <= hidden_layer_nodes_1",
+    "hidden_layer_nodes_3 <= hidden_layer_nodes_2",
+]
+
+o_constraints = None
 
 ax_client.create_experiment(
-    name="sampler_testing",
-    parameters=MNIST_parameters,
-    objectives=MNIST_single_objective,
+    name="regression_testing",
+    parameters=Superconductivity_parameters,
+    objectives=Superconductivity_single_objective,
     parameter_constraints=p_constraints,
     outcome_constraints=o_constraints,
 )
