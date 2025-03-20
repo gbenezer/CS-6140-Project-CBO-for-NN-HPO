b46768bfbb566c2923e4aa3d7cc40e0713d703d0
Gil Benezer <gil.benezer@gmail.com>
2025-03-20 00:26:24 -0400

    bunch of imports, plug-and-play acquisition function (tested and good), global early stopping added (does not break code but remains untested), early stopping based on TensorBoardMetric "training_data" (untested)

--- src/Ax_BO/conduct_experiment.py
+++ src/Ax_BO/conduct_experiment.py
@@ -3,18 +3,21 @@
 # standard imports
 from pathlib import Path
 import os
-from ax.modelbridge.registry import ModelRegistryBase, Models
-from ax.modelbridge.dispatch_utils import choose_generation_strategy
+from typing import Literal
+from ax.modelbridge.registry import Models
+from ax.modelbridge.generation_strategy import GenerationStep, GenerationStrategy
 from ax.service.ax_client import AxClient
+from ax.early_stopping.strategies import PercentileEarlyStoppingStrategy, ThresholdEarlyStoppingStrategy
 from ax.global_stopping.strategies import ImprovementGlobalStoppingStrategy
 from ax.exceptions.core import OptimizationShouldStop
+from ax.metrics.tensorboard import TensorboardMetric
+from tensorboard.backend.event_processing import plugin_event_multiplexer as event_multiplexer
 
 
 from ax.plot.contour import interact_contour
 from ax.plot.diagnostic import interact_cross_validation
-from ax.plot.scatter import interact_fitted, plot_objective_vs_constraints, tile_fitted
+from ax.plot.scatter import interact_fitted, plot_objective_vs_constraints
 from ax.modelbridge.cross_validation import cross_validate
-from ax.plot.slice import plot_slice
 import plotly.io as pio
 
 # adding all the modules and submodules to the path
@@ -23,19 +26,18 @@ import sys
 sys.path.insert(0, "C:/Users/Gil/Documents/Repositories/Python/CS_6140/Project")
 
 from src.network.evaluate_network import evaluate_hyperparameters
-import src.Ax_BO.threshold_global_stopping as tgs
+from src.Ax_BO.threshold_global_stopping import ThresholdGlobalStoppingStrategy
 
-
-# TODO: figure out how to add tracking metrics for the other metrics I am tracking
-# with AxClient.add_tracking_metrics
 def conduct_experiment(
-    task,
+    task: Literal["regression", "classification"],
     parameter_space,
-    search_space,
     objective,
     param_constraints,
     out_constraints,
     tracking_metrics,
+    acquisition_func_class,
+    train_loss_threshold,
+    test_loss_threshold,
     train_loader,
     valid_loader,
     test_loader,
@@ -50,6 +52,7 @@ def conduct_experiment(
     fully_random,
     interactive_plots,
     global_early_stop,
+    trial_early_stop,
     seed,
 ):
 
@@ -58,30 +61,71 @@ def conduct_experiment(
     if not os.path.exists(plot_directory):
         plot_directory.mkdir(parents=True)
 
-    # if the trial is supposed to be fully random, force the AxClient to use fully random search
+    # the minimum number of initialization trials should be twice the parameter space dimension
+    # (guideline heuristic in BoTorch is max(5, 2 * len(parameter_space))), and all the parameter spaces
+    # I am looking at are higher dimensional by a factor of 2
+    min_number_initialization_trials = 2 * len(parameter_space)
+
+    # if the trial is supposed to be fully random, force the AxClient to only use fully quasi-random search
     if fully_random:
-        gen_strat = choose_generation_strategy(
-            search_space=search_space,
-            random_seed=seed,
-            force_random_search=fully_random,
+        gen_strat = GenerationStrategy(
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,  # quasi-random space filling generation
+                    num_trials=-1,  # unlimited number of trials can be generated
+                )
+            ],
+            name="quasi_random_Sobol",
         )
     else:
-        gen_strat = None
-
-    # TODO: configure threshold parameter
-    # TODO: actually implement for early stopping
-    if global_early_stop:
-        gss = tgs.ThresholdGlobalStoppingStrategy(
-            min_trials=1, threshold=0.95
+        gen_strat = GenerationStrategy(
+            steps=[
+                GenerationStep(
+                    model=Models.SOBOL,  # quasi-random space filling generation
+                    num_trials=min_number_initialization_trials,  # minimum and maximum are the same for this strategy
+                    min_trials_observed=min_number_initialization_trials,
+                ),
+                GenerationStep(
+                    model=Models.BOTORCH_MODULAR,
+                    num_trials=-1,  # unlimited
+                    model_kwargs={  # Kwargs to pass to `BoTorchModel.__init__`
+                        "botorch_acqf_class": acquisition_func_class,
+                    },
+                ),
+            ],
+            name="custom_acquisition_function_and_model",
         )
+
+    # TODO: allow configuration of threshold parameters
+    # NOTE: This will break for multi-objective problems
+    if global_early_stop and task == "classification":
+        # 95% test accuracy threshold
+        gss = ThresholdGlobalStoppingStrategy(min_trials=5, threshold=0.95)
+    elif global_early_stop and task == "regression":
+        # 0.2 range-normalized root mean square deviation
+        gss = ThresholdGlobalStoppingStrategy(min_trials=5, threshold=0.2)
     else:
         gss = None
+        
+    if trial_early_stop:
+        # good threshold for MNIST looks to be 5,
+        # yet to be determined for Superconductivity
+        ess = ThresholdEarlyStoppingStrategy(metric_names=["training_loss"],
+                                             metric_threshold=train_loss_threshold,
+                                             min_progression=0.1,
+                                             min_curves=1,
+                                             normalize_progressions=True)
+    else:
+        ess = None
 
     ax_client = AxClient(
         generation_strategy=gen_strat,
         random_seed=seed,
         global_stopping_strategy=gss,
+        early_stopping_strategy=ess
     )
+    
+    # ax_client.should_stop_trials_early
 
     # create the experiment
     ax_client.create_experiment(
@@ -93,15 +137,24 @@ def conduct_experiment(
     )
 
     ax_client.add_tracking_metrics(tracking_metrics)
+    
+    # method to add TensorBoard metric for early stopping
+    if trial_early_stop:
+        ax_client.experiment.add_tracking_metric(TensorboardMetric(name="training_loss",
+                                                                tag="training_loss",
+                                                                lower_is_better=True))
 
     # run the optimization loop
-    # TODO: implement logic to switch out acquisition functions
     for _ in range(max_trials):
-        parameterization, trial_index = ax_client.get_next_trial()
+        
+        try:
+            parameterization, trial_index = ax_client.get_next_trial()
+        except OptimizationShouldStop as exc:
+            print(exc.message)
+            break
+        
         trial_log_dir_name = experiment_name + "/" + f"trial_{trial_index}"
-        ax_client.complete_trial(
-            trial_index=trial_index,
-            raw_data=evaluate_hyperparameters(
+        trial_data = evaluate_hyperparameters(
                 task=task,
                 train_loader=train_loader,
                 valid_loader=valid_loader,
@@ -114,8 +167,27 @@ def conduct_experiment(
                 num_rep=num_reps_per_trial,
                 max_epochs=max_epochs,
                 parameterization=parameterization,
-            ),
         )
+        
+        # test whether or not there are absurd test loss values
+        if trial_data["mean_test_loss"] > test_loss_threshold:
+            ax_client.abandon_trial(trial_index=trial_index, reason="mean test loss too high")
+            continue
+        
+        # TODO: test SEM toggle output because of this
+        # NOTE: When ``raw_data`` does not specify SEM for a given metric, Ax
+        # will default to the assumption that the data is noisy (specifically,
+        # corrupted by additive zero-mean Gaussian noise) and that the
+        # level of noise should be inferred by the optimization model.
+        
+        try:
+            ax_client.complete_trial(
+                trial_index=trial_index,
+                raw_data=trial_data,
+            )
+        except ValueError:
+            ax_client.abandon_trial(trial_index=trial_index, reason="invalid metric values")
+            continue
 
     # getting results of experiment
     experiment_df = ax_client.experiment.to_df()
